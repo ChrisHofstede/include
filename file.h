494980e3d4962c1a92a4bf57e0b1191e913e8082
Chris Hofstede <chris-hofstede@outlook.com>
2016-01-02 23:20:32 +0100

    Convert to GNU compiler but untested

--- file.h
+++ file.h
@@ -1,121 +1,103 @@
 #ifndef FILE_H
 #define FILE_H
+#include <windows.h>
+#include <bstring.h>
 
 namespace b {
 
-template <class T> class TFile
-{
+template<class T> class TFile {
 	T* file_name;
 public:
 	static const T separator[];
-	TFile() : file_name(0) {}
-	TFile(const T* file)
-	{
+	TFile() :
+			file_name(0) {
+	}
+	TFile(const T* file) {
 		file_name = strnewdup(file);
 	}
 	TFile(const T* parent, const T* child);
-	TFile(const TFile& e)
-	{
+	TFile(const TFile& e) {
 		file_name = strnewdup(e.file_name);
 	}
-	~TFile()
-	{
+	~TFile() {
 		delete[] file_name;
 	}
-	TFile& operator = (T* ptr)
-	{
+	TFile& operator =(T* ptr) {
 		delete[] file_name;
 		file_name = strnewdup(ptr);
 		return *this;
 	}
-	operator bool ()
-	{
+	operator bool() {
 		return file_name;
 	}
-	operator const T* ()
-	{
+	operator const T*() {
 		return file_name;
 	}
-	T** operator & ()
-	{
+	T** operator &() {
 		delete[] file_name;
 		file_name = 0;
 		return &file_name;
 	}
-	T* GetBuffer(int length = MAX_PATH)
-	{
+	T* GetBuffer(int length = MAX_PATH) {
 		delete[] file_name;
 		file_name = new T[length];
 		return file_name;
 	}
-	const T* GetPath()
-	{
+	const T* GetPath() {
 		return file_name;
 	}
 	const T* GetName();
 	TFile<T> GetParent();
-	static bool Exists(const T* file)
-	{
+	static bool Exists(const T* file) {
 		return GetFileAttributes(file) != INVALID_FILE_ATTRIBUTES;
 	}
-	inline bool Exists()
-	{
+	inline bool Exists() {
 		return Exists(file_name);
 	}
-	bool IsDirectory()
-	{
+	bool IsDirectory() {
 		return GetFileAttributes(file_name) & FILE_ATTRIBUTE_DIRECTORY;
 	}
-	bool Delete()
-	{
+	bool Delete() {
 		return DeleteFile(file_name);
 	}
-	bool Copy(const T* dest)
-	{
+	bool Copy(const T* dest) {
 		return CopyFile(file_name, dest);
 	}
-	bool Move(const T* dest)
-	{
+	bool Move(const T* dest) {
 		return MoveFile(file_name, dest);
 	}
-	bool MakeDirectory()
-	{
+	bool MakeDirectory() {
 		return CreateDirectory(file_name, 0);
 	}
-	bool DeleteDirectory()
-	{
+	bool DeleteDirectory() {
 		return RemoveDirectory(file_name);
 	}
 	bool MakeDirectories();
 };
 
-template <class T> const T TFile<T>::separator[] = { T('\\'), T('\0') };
+template<class T> const T TFile<T>::separator[] = { T('\\'), T('\0') };
 
-template <class T> TFile<T>::TFile(const T* parent, const T* child)
-{
+template<class T> TFile<T>::TFile(const T* parent, const T* child) {
 	int len = strlen<T>(parent);
 	bool add_separator = len > 0 && parent[len - 1] != separator[0];
 	len += (add_separator) ? 2 : 1;
 	file_name = new T[len + strlen<T>(child)];
 	strcpy<T>(file_name, parent);
-	if(add_separator)
-	{
+	if (add_separator) {
 		strcat<T>(file_name, separator);
 	}
 	strcat<T>(file_name, child);
 }
 
-template <class T> const T* TFile<T>::GetName()
-{
+template<class T> const T* TFile<T>::GetName() {
 	const T* ptr = b::strrchr(file_name, separator[0]);
 	return (ptr) ? ptr + 1 : file_name;
 }
 
-template <class T> TFile<T> TFile<T>::GetParent()
-{
+template<class T> TFile<T> TFile<T>::GetParent() {
 	TFile<T> Parent;
 	const T* ptr = b::strrchr(file_name, separator[0]);
-	if(ptr) {
+	if (ptr) {
 		int len = ptr - file_name;
 		Parent.file_name = new T[len + 1];
 		b::copy(Parent.file_name, file_name, len);
@@ -124,43 +106,27 @@ template <class T> TFile<T> TFile<T>::GetParent()
 	return Parent;
 }
 
-template <class T> bool TFile<T>::MakeDirectories()
-{
+template<class T> bool TFile<T>::MakeDirectories() {
 	ASSERT(file_name);
-	T* path = 0;
-	T* dir_path = 0;
-	try
-	{
-		path = b::strnewdup(file_name);
-		dir_path = new T[b::strlen(file_name) + 2]; // Include ending zero and separator
-		dir_path[0] = 0;
-		T* ptr = b::strtok(path, separator);
-		while(ptr)
-		{
-			b::strcat(dir_path, ptr);
-			b::strcat(dir_path, separator);
-			DWORD dwResult = GetFileAttributes(dir_path);
-			if(dwResult == INVALID_FILE_ATTRIBUTES)
-			{
-				if(!CreateDirectory(dir_path, 0))
-				{
-					break;
-				}
+	TString<T> path(file_name);
+	TString<T> dir_path;
+	dir_path.set_buffer(path.length() + 1); // Include ending zero and separator
+	dir_path[0] = 0;
+	T* ptr = b::strtok(path, separator);
+	while (ptr) {
+		b::strcat(dir_path, ptr);
+		b::strcat(dir_path, separator);
+		DWORD dwResult = GetFileAttributes(dir_path);
+		if (dwResult == INVALID_FILE_ATTRIBUTES) {
+			if (!CreateDirectory(dir_path, 0)) {
+				break;
 			}
-			else
-			{
-				if((dwResult & FILE_ATTRIBUTE_DIRECTORY) == 0)
-				{
-					break;
-				}
+		} else {
+			if ((dwResult & FILE_ATTRIBUTE_DIRECTORY) == 0) {
+				break;
 			}
-			ptr = b::strtok((T*)0, separator);
 		}
-	}
-	__finally
-	{
-		delete[] path;
-		delete[] dir_path;
+		ptr = b::strtok((T*) 0, separator);
 	}
 	return IsDirectory();
 }
