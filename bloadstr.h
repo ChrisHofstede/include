f50dc05c65ad9ce705f0504386b0266c07ec0459
Chris Hofstede <chris-hofstede@outlook.com>
2016-01-06 19:32:15 +0100

    Add LoadString template

---
+++ bloadstr.h
@@ -0,0 +1,32 @@
+/*
+ * loadstr.h
+ *
+ *  Created on: 5 jan. 2016
+ *      Author: chris
+ */
+
+#ifndef BLOADSTR_H_
+#define BLOADSTR_H_
+#include <bstring.h>
+#include <windows.h>
+
+namespace b {
+
+// Assigns the uID string resource to wsDest, returns length (0 if no resource)
+template<class T> int LoadString(TString<T>& wsDest, UINT uID,
+		HINSTANCE hInstance = ::GetModuleHandle(NULL)) {
+	PTCHAR wsBuf; // no need to initialize
+	wsDest.clear();
+	if (size_t len = ::LoadString(hInstance, uID, (PTCHAR) &wsBuf, 0))
+		wsDest = wsBuf;
+	return wsDest.length();
+}
+
+// Returns a StringType with uID string resource content (empty if no resource)
+template<class T> TString<T> LoadString(UINT uID, HINSTANCE hInstance) {
+	TString<T> sDest;
+	return LoadString(sDest, uID, hInstance) ? sDest : TString<T>();
+}
+
+} // End Namespace
+#endif /* LOADSTR_H_ */
